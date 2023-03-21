e60ef4ff8f158efa291b922504f6fee18148597d
nicolas <nicolasfernandez84@gmail.com>
2023-03-20 21:07:39 -0300

    mensaje de confirmacion

---
+++ pruebaCon Ant/src/proyectoeggnico/EjercicioDePruebaNico.java
@@ -0,0 +1,35 @@
+
+package proyectoeggnico;
+
+import java.util.Scanner;
+
+
+public class EjercicioDePruebaNico {
+    public static void main(String[] args) {
+     
+        Scanner leer = new Scanner(System.in);
+
+        System.out.println("escriba dos numeros");
+
+      int num1 = leer.nextInt();
+
+        int num2 = leer.nextInt();
+
+        int resto = num1 + num2;
+        
+        System.out.println(num1  + " + " + num2 + " es " + resto );
+
+     
+    
+    
+        
+        
+        
+        
+        
+        
+  
+        
+    }
+    
+}
